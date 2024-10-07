dc2733174b6b5f475d95693c1e3eb5e03084239e
Erkin Dilekci <dilekcierkin@gmail.com>
2024-10-07 17:52:54 +0300

    Add repository annotation

--- src/main/java/com/erkindilekci/booksapi/repository/BookRepository.java
+++ src/main/java/com/erkindilekci/booksapi/repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.erkindilekci.booksapi.repository;
 import com.erkindilekci.booksapi.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, String>, PagingAndSortingRepository<Book, String> {
 }
